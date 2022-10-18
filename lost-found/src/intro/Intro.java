f49a9a7c996c65dd925f05370fbfba5e374cb558
김하늘 <80873494+hanul1206@users.noreply.github.com>
2022-10-18 12:57:47 +0900

    Disappears after 3 seconds

--- lost-found/src/intro/Intro.java
+++ lost-found/src/intro/Intro.java
@@ -1,29 +1,36 @@
 package intro;
 
-import java.awt.Graphics;
-import java.awt.Image;
-
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
+import java.awt.*;
+import javax.swing.*;
 
 public class Intro extends JFrame {
-   private Image background=new ImageIcon(Intro.class.getResource("../image/intro.png")).getImage();
-
+   private Image background=new ImageIcon(Intro.class.getResource("../image/first.png")).getImage();
+   
    public Intro() {
       setSize(600, 900);
       setResizable(false);
       setLocationRelativeTo(null);
       setVisible(true);
       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+      try{
+
+         Thread.sleep(3000);
+
+      }catch(InterruptedException ie){
+
+         System.out.println(ie.getMessage());
+
+      }
+
+      setVisible(false);
    }
 
    public void paint(Graphics g) {
       g.drawImage(background, 0, 0, null);
    }
-
+   
    public static void main(String[] args) {
       new Intro();
-
    }
-
 }
